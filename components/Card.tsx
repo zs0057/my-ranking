d86114b7d6f7b1793589afdd932918f5345598dd
zs0057 <soss4823@gmail.com>
2024-07-23 23:42:21 +0900

    plz sleep

--- components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import useStore from "../lib/store";
-
-const Card: React.FC = () => {
-  const bears = useStore((state) => state.bears);
-  return <div className="ss">{bears}</div>;
-};
-
-export default Card;
